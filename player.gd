2546ae7753aca95392085de09b8d20a235d29c2d
wdthor <wdthor@proton.me>
2023-05-02 01:13:58 +0200

    9- Number operations & more printing

--- player.gd
+++ player.gd
@@ -3,9 +3,8 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	test()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func test():
+	print((2 + 3) * 5)
+	print('2 + 2 is ' + str(4))
