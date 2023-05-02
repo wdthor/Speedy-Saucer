076389d081e77a26e0b4496d783c549abb6fbf24
wdthor <wdthor@proton.me>
2023-05-02 07:00:20 +0200

    11- Function parameters / arguments

--- player.gd
+++ player.gd
@@ -3,8 +3,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	test()
+	printDoubled(10)
 
-func test():
-	print((2 + 3) * 5)
-	print('2 + 2 is ' + str(4))
+func printDoubled(input_number):
+	print(input_number * 2)
