1e3c82cd56ba31ac29edb053815dd4c29ebe08f7
wdthor <wdthor@proton.me>
2023-05-03 00:46:07 +0200

    14- If statement & comparison operators

--- player.gd
+++ player.gd
@@ -4,7 +4,14 @@ extends RigidBody2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# apply_impulse(Vector2(100, -155))
-	pass
+	var age = "young"
+	if age == "young":
+		print("You are young")
+	elif(age == "adult"):
+		print("You are an adult")
+	else:
+		print("I don't know your age")		
+	
 
 func _process(delta):
 	pass
