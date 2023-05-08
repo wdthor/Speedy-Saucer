f7555b79dcf0143cc1ad0ffaf4b99ee4961388be
wdthor <wdthor@proton.me>
2023-05-08 13:09:10 +0200

    Adding player rotation when moving

--- player.gd
+++ player.gd
@@ -1,13 +1,18 @@
 extends RigidBody2D
 
 var force = 500
+var rotation_force = 15
 
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force,0))
+		rotate(rotation_force)
 	if Input.is_action_pressed("move_left"):
 		apply_force(Vector2(-force,0))
+		rotate(-rotation_force)
 	if Input.is_action_pressed("move_up"):
 		apply_force(Vector2(0,-force))
+		rotate(rotation_force)
 	if Input.is_action_pressed("move_down"):
 		apply_force(Vector2(0,force))
+		rotate(-rotation_force)		
